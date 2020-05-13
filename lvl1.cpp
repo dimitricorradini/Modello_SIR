6f931f379b972348aff5de1b1d3e5a359d4ac6ad
Cattol00 <niki00cat@gmail.com>
2020-05-13 18:16:06 +0200

    ritocchi belli

--- lvl1.cpp
+++ lvl1.cpp
@@ -10,77 +10,66 @@ struct State
     int I;
 };
 
-class Graph{
+class Graph
+{
+	  private:
 
     State start_;
     double Beta_;
     double Gamma_;
-    //else a State vector and an int(?)
+
     public:
     
     Graph(State start, double Beta, double Gamma)
-    : start_{start}, Beta_{Beta}, Gamma_{Gamma}{
-    //gamma, beta must be below one
-        if (Beta>1||Gamma>1||Beta<0||Gamma<0) {
+    : start_{start}, Beta_{Beta}, Gamma_{Gamma} {
+        if (Beta > 1 || Gamma > 1 || Beta < 0 || Gamma < 0) {
             throw std::runtime_error{"Coefficients Beta and Gamma must be between 0 and 1"}; 
-        };
-    };
+        }
+    }
 
-    std::vector<State> creategraph(int sim_length) {
+    std::vector<State> creategraph() {
         std::vector<State> SRIgraph_{start_};
-        double N = start_.S+start_.I+start_.R;
-        for (int i=0; i!= sim_length; i++) {
+        double N = start_.S + start_.I + start_.R;
+        while (true) {
             auto const& current = SRIgraph_.back();
             State next;
-            double nextS=current.S-Beta_*current.I*current.S;
-            double nextR=current.R+Gamma_*current.I;
-            //appprossimo!
-            
-            if (nextS-floor(nextS)>0.5){
-                next.S=ceil(nextS);
-            } else {
-                next.S=floor(nextS);
-            };
+           
+            next.S = floor(current.S-Beta_*current.I*current.S);
+            next.R = ceil(current.R+Gamma_*current.I);
 
-            if (nextR-floor(nextR)>=0.5){
-                next.R=ceil(nextR);
-            } else {
-                next.R=floor(nextR);
-            };
-            
-            //what if the virus stops mid-way???
-            if (next.S<0){ 
-                next.S=0;
-            };
-            
-            next.I=N-next.R-next.S;           
+            if (next.S < 0){ 
+              next.S = 0;
+            }
+
+            next.I = N - next.R - next.S;           
             assert(next.S+next.I+next.R == N);      
             SRIgraph_.push_back(next);
+            if (next.I == 0) { break; }
         }
         return SRIgraph_;
-    };   
+    }   
 };
 
-void printraw(std::vector<State> myGraph){ //must improve
-    int day = 1;
-    	for (auto const& state : myGraph){
-            std::cout << "Day "<< day << "\n";
-    	    std::cout << "Number of susceptible people: " << state.S << "\n";
-            std::cout << "Number of infected people: " << state.I << "\n";
-            std::cout << "Number of recovered and/or dead people: " << state.R << "\n";
-            day++;
-    	};
-    assert(day==myGraph.size());
-};
+void printraw(std::vector<State> myGraph)
+{
+	int day = 0;
+	for (auto const& state : myGraph) {
+    std::cout << "Day "<< day + 1 << "\n";
+    std::cout << "Number of susceptible people: " << state.S << "\n";
+    std::cout << "Number of infected people: " << state.I << "\n";
+    std::cout << "Number of recovered and/or dead people: " << state.R << "\n";
+    ++day;
+  }
+  assert(day == myGraph.size());
+}
 
-
-int main(){
-    //definisco stato a t_0
+int main()
+{
     State t_0{};
     t_0.S = 1000;
     t_0.I = 1;
     Graph disease(t_0, 0.3, 0.1);
-    auto SRIgraph = disease.creategraph(100);
+    auto SRIgraph = disease.creategraph();
     printraw(SRIgraph);
   
 }
