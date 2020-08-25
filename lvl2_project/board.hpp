e28359512debe6d8ccbba5573f0d86a55db99ae8
dimitricorradini <64958902+dimitricorradini@users.noreply.github.com>
2020-08-25 16:54:53 +0200

    Update board.hpp

--- lvl2_project/board.hpp
+++ lvl2_project/board.hpp
@@ -21,17 +21,17 @@ private:
     std::vector<State> m_board;
 
 public:
-    Board(int side) : m_size(side), board_(side * side) {}
+    Board(int side) : m_side(side), m_board(side * side) {}
 
     State operator()(int i, int j) const
     {
-        return (i >= 0 && i < n_ && j >= 0 && j < n_) ? board_[i * n_ + j] : State::Rec;
+        return (i >= 0 && i < m_side && j >= 0 && j < m_side) ? m_board[i * m_side + j] : State::Rec;
     }
 
     State &operator()(int i, int j)
     {
-        assert(i >= 0 && i < n_ && j >= 0 && j < n_);
-        return board_[i * n_ + j];
+        assert(i >= 0 && i < m_side && j >= 0 && j < m_side);
+        return board_[i * m_side + j];
     }
 
     int size() const { return m_side; }
