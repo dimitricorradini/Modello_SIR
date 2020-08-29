5a1a11ab05b64d895c8006501814b0fa7f380d45
TassoRosso <65049446+TassoRosso@users.noreply.github.com>
2020-08-29 18:11:22 +0200

    Update main.cpp

--- level2/main.cpp
+++ level2/main.cpp
@@ -9,7 +9,72 @@
 
 int main()
 {
-  int dim = 50;
+  int dim;
+  double infection_rate;
+  double recovery_rate;
+  double mortality;
+  char first_choose;
+  std::cout << "Insert board dimension (1 to 100): ";
+  std::cin >> dim;
+  std::cout << "Press S to start the simulation or C to choose your parameters :";
+  std::cin >> first_choose;
+  while(first_choose != 'S' || first_choose != 's'|| first_choose != 'C' || first_choose != 'c')
+  {
+    std::cout << "Press S to start the simulation or C to choose your parameters :";
+    std::cin >> first_choose;
+
+    switch (first_choose)
+    {
+    case ('s' , 'S'):
+    {
+      infection_rate = 0.6;
+      recovery_rate = 0.4;
+      mortality = 0.1;
+      std::cout << "Enjoy!" << '\n';
+      std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    }
+    case ('c' , 'C') :
+    {
+      std::cout << "Insert infection rate (0 to 1): ";
+      std::cin >> infection_rate;
+      std::cout << "Insert recovery rate (0 to 1): ";
+      std::cin >> recovery_rate;
+      std::cout << "Insert mortality (0 to 1): ";
+      std::cin >> mortality;
+      std::cout << "Enjoy!" << '\n';
+      std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    }
+      break;
+    
+    default:
+    {
+      std::cout << "Please type C or S!" << '\n';
+    }
+      break;
+    }
+    /*
+    if(first_choose == 's'|| first_choose == 'S')
+    {
+      infection_rate=0.6;
+      recovery_rate=0.4;
+      mortality=0.1;
+      std::cout<<"Enjoy!"<<'\n';
+      std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    }
+    else if(first_choose == 'C' || first_choose == 'c')
+    {
+      std::cout<<"Insert infection rate (0 to 1): ";
+      std::cin>>infection_rate;
+      std::cout<<"Insert recovery rate (0 to 1): ";
+      std::cin>>recovery_rate;
+      std::cout<<"Insert mortality (0 to 1): ";
+      std::cin>>mortality;
+      std::cout<<"Enjoy!"<<'\n';
+      std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    }
+    */
+  }
+  
   SIR::Board board(dim);
   std::vector<SIR::Points> g_points;
   SIR::Info info{};
