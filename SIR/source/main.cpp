8157d49e07682d0e09506f9dddbe3468f01f4558
TassoRosso <65049446+TassoRosso@users.noreply.github.com>
2020-08-30 18:02:10 +0200

    Update main.cpp

--- SIR/source/main.cpp
+++ SIR/source/main.cpp
@@ -1,61 +1,51 @@
-#include <chrono>
-#include <thread>
 #include "board.hpp"
 #include "display.hpp"
-#include "points.hpp"
 #include "graph.hpp"
 #include "info.hpp"
+#include "points.hpp"
+#include <chrono>
+#include <thread>
 
-int main()
-{
+int main() {
   int dim = 0;
   double infection_rate;
   double recovery_rate;
   double mortality;
   char choice;
-  while (dim > 50 || dim < 1)
-  {
+  while (dim > 50 || dim < 1) {
     std::cout << '\n' << "Insert board dimension (1 to 50): ";
     std::cin >> dim;
   }
-  while(choice != 'S' && choice != 'C')
-  {
-    std::cout << '\n' << "Press S to start the simulation or C to choose your parameters: ";
+  while (choice != 'S' && choice != 'C') {
+    std::cout
+        << '\n'
+        << "Press S to start the simulation or C to choose your parameters: ";
     std::cin >> choice;
     choice = toupper(choice);
-    switch (choice)
-    {
-      case ('S'):
-      {
-        infection_rate = 0.6;
-        recovery_rate = 0.4;
-        mortality = 0.1;
-        std::cout << '\n' << "Enjoy!" << '\n';
-        std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-      }
-        break;
-      case ('C'):
-      {
-        std::cout << '\n';
-        std::cout << "Insert infection rate (0 to 1): ";
-        std::cin >> infection_rate;
-        std::cout << "Insert recovery rate (0 to 1): ";
-        std::cin >> recovery_rate;
-        std::cout << "Insert mortality (0 to 1): ";
-        std::cin >> mortality;
-        std::cout << '\n' << "Enjoy!" << '\n';
-        std::this_thread::sleep_for(std::chrono::milliseconds(1500));
-      }
-        break;
-    
-      default:
-      {
-        std::cout << '\n' << "Please type C or S!" << '\n';
-      }
-        break;
+    switch (choice) {
+    case ('S'): {
+      infection_rate = 0.6;
+      recovery_rate = 0.4;
+      mortality = 0.1;
+      std::cout << '\n' << "Enjoy!" << '\n';
+      std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    } break;
+    case ('C'): {
+      std::cout << '\n';
+      std::cout << "Insert infection rate (0 to 1): ";
+      std::cin >> infection_rate;
+      std::cout << "Insert recovery rate (0 to 1): ";
+      std::cin >> recovery_rate;
+      std::cout << "Insert mortality (0 to 1): ";
+      std::cin >> mortality;
+      std::cout << '\n' << "Enjoy!" << '\n';
+      std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+    } break;
+
+    default: { std::cout << '\n' << "Please type C or S!" << '\n'; } break;
     }
   }
-  
+
   SIR::Board board(dim);
   std::vector<SIR::Points> g_points;
   SIR::Info info{};
@@ -72,8 +62,7 @@ int main()
 
   display.draw(board);
 
-  while (p.inf != 0)
-  {
+  while (p.inf != 0) {
     board = SIR::evolve(board, infection_rate, recovery_rate, mortality);
     display.draw(board);
     p = SIR::count(board);
@@ -89,3 +78,4 @@ int main()
   graph.closing();
   std::cout << '\n' << "Thanks for playing!" << '\n';
 }
+
