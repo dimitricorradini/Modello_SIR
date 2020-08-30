26ec306080594804aa5ad8e2f472ef78b1ce1350
TassoRosso <65049446+TassoRosso@users.noreply.github.com>
2020-08-30 18:03:32 +0200

    Update points.hpp

--- SIR/source/points.hpp
+++ SIR/source/points.hpp
@@ -1,24 +1,22 @@
 #ifndef SIR_POINTS_HPP
 #define SIR_POINTS_HPP
 
-namespace SIR 
-{
-  class Board;
+namespace SIR {
+class Board;
 
-  static constexpr int heightG = 700;
-  static constexpr int widthG = 700;    
-    
-  struct Points
-  {
-    int sus;
-    int rec;
-    int inf;
-    int dead;
-  };
+static constexpr int heightG = 700;
+static constexpr int widthG = 700;
 
-  Points count(Board const& board);
+struct Points {
+  int sus;
+  int rec;
+  int inf;
+  int dead;
+};
 
-  Points convert(Board const& board, Points const& point);
-}
+Points count(Board const &board);
+
+Points convert(Board const &board, Points const &point);
+} // namespace SIR
 
 #endif
