66635f6af7c53fe689f6da739cfa6efe46f3bc41
TassoRosso <65049446+TassoRosso@users.noreply.github.com>
2020-08-30 17:57:34 +0200

    Update board.hpp

--- SIR/source/board.hpp
+++ SIR/source/board.hpp
@@ -1,55 +1,44 @@
 #ifndef SIR_BOARD_HPP
 #define SIR_BOARD_HPP
 
-#include <vector>
 #include <cassert>
 #include <iostream>
+#include <vector>
 
-namespace SIR 
-{
-  enum class State
-  {
-    Susc,
-    Inf,
-    Rec,
-    Dead
-  };
+namespace SIR {
+enum class State { Susc, Inf, Rec, Dead };
 
-  class Board
-  {
-    private:
-    int m_side;
-    std::vector<State> m_board;
+class Board {
+private:
+  int m_side;
+  std::vector<State> m_board;
 
-    public:
-    Board(int side) : m_side(side), m_board(side * side) 
-    {
-      if (side <= 0)
-      {
-        throw std::runtime_error{"There is no population"};
-      } 
-      if (side > 50)
-      {
-        throw std::runtime_error{"Too big to fit on the screen"};
-      }
+public:
+  Board(int side) : m_side(side), m_board(side * side) {
+    if (side <= 0) {
+      throw std::runtime_error{"There is no population"};
     }
-
-    State operator()(int i, int j) const
-    {
-      return (i >= 0 && i < m_side && j >= 0 && j < m_side) ? m_board[i * m_side + j] : State::Rec;
+    if (side > 50) {
+      throw std::runtime_error{"Too big to fit on the screen"};
     }
+  }
 
-    State& operator()(int i, int j)
-    {
-      assert(i >= 0 && i < m_side && j >= 0 && j < m_side);
-      return m_board[i * m_side + j];
-    }
+  State operator()(int i, int j) const {
+    return (i >= 0 && i < m_side && j >= 0 && j < m_side)
+               ? m_board[i * m_side + j]
+               : State::Rec;
+  }
 
-    int side() const { return m_side; }
-  };
+  State &operator()(int i, int j) {
+    assert(i >= 0 && i < m_side && j >= 0 && j < m_side);
+    return m_board[i * m_side + j];
+  }
 
-  Board evolve(Board const& current, double const beta, double const gamma, double const mu);
-}
+  int side() const { return m_side; }
+};
+
+Board evolve(Board const &current, double const beta, double const gamma,
+             double const mu);
+} // namespace SIR
 
 #endif
-
