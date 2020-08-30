f4f0ec7ebbf0569aef993d3cadd8c093dde2a407
TassoRosso <65049446+TassoRosso@users.noreply.github.com>
2020-08-30 17:58:54 +0200

    Update display.hpp

--- SIR/source/display.hpp
+++ SIR/source/display.hpp
@@ -3,35 +3,33 @@
 
 #include <SFML/Graphics.hpp>
 
-namespace SIR
-{
-  class Board;
+namespace SIR {
+class Board;
 
-  class Display
-  {
-    private:
-    int const m_board_side;
-	  sf::RenderWindow m_window;
+class Display {
+private:
+  int const m_board_side;
+  sf::RenderWindow m_window;
 
-	  static constexpr int cell_side = 15;
+  static constexpr int cell_side = 15;
 
-	  static constexpr auto s_window_title = "Virus simulation";
+  static constexpr auto s_window_title = "Virus simulation";
 
-	  public:
-	  Display(int board_side)
-	    : m_board_side{board_side}, m_window { 
-											  sf::VideoMode(m_board_side * cell_side, m_board_side * cell_side),
-											  s_window_title,
-											  sf::Style::Titlebar} {}
+public:
+  Display(int board_side)
+      : m_board_side{board_side}, m_window{
+                                      sf::VideoMode(m_board_side * cell_side,
+                                                    m_board_side * cell_side),
+                                      s_window_title, sf::Style::Titlebar} {}
 
-	  void wait_key_pressed();
+  void wait_key_pressed();
 
-	  void closing();
+  void closing();
 
-	  void click(Board& board);
+  void click(Board &board);
 
-    int draw(Board const& board);
-  };
-}
+  int draw(Board const &board);
+};
+} // namespace SIR
 
 #endif
